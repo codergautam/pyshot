8bf9d31b7b2ce41fb045a74548be0d0376e09ae1
Coder Gautam <gautamgxtv@gmail.com>
2021-09-07 17:22:30 -0500

    bullets

--- game.py
+++ game.py
@@ -12,6 +12,32 @@ import os
 pygame.init()
 window = pygame.display.set_mode((1280, 720))
 player = pygame.transform.smoothscale(pygame.image.load("square.png").convert_alpha(), (100, 100))
+    
+class Bullet:
+    def __init__(self, x, y):
+        self.pos = (x, y)
+        mx, my = pygame.mouse.get_pos()
+        self.dir = (mx - x, my - y)
+        length = math.hypot(*self.dir)
+        if length == 0.0:
+            self.dir = (0, -1)
+        else:
+            self.dir = (self.dir[0]/length, self.dir[1]/length)
+        angle = math.degrees(math.atan2(-self.dir[1], self.dir[0]))
+
+        self.bullet = pygame.Surface((10, 4)).convert_alpha()
+        self.bullet.fill((0, 0, 0))
+        self.bullet = pygame.transform.rotate(self.bullet, angle)
+        self.speed = 10
+
+    def update(self):  
+       
+        self.pos = (self.pos[0]+self.dir[0]*self.speed, 
+                   self.pos[1]+self.dir[1]*self.speed)
+
+    def draw(self, surf):
+        bullet_rect = self.bullet.get_rect(center = self.pos)
+        surf.blit(self.bullet, bullet_rect)  
 
 def rotate(x,y):
     correction_angle = 90
@@ -35,7 +61,7 @@ def rotate(x,y):
     if(y<620):
         if key[pygame.K_s]:
             player_rect.move_ip(0, speed)
-
+    
     mx, my = pygame.mouse.get_pos()
     dx, dy = mx - player_rect.centerx, my - player_rect.centery
     angle = math.degrees(math.atan2(-dy, dx)) - correction_angle
@@ -43,20 +69,34 @@ def rotate(x,y):
     rot_image = pygame.transform.rotate(player, angle)
     rot_image_rect = rot_image.get_rect(center=player_rect.center)
 
-    window.fill((255, 255, 255))
+
     window.blit(rot_image, rot_image_rect.topleft)
     return((player_rect.left, player_rect.top))
 
 
-
+bullets = []
 run = True
 x = 0
 y = 0
 while run:
+    window.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+            bullets.append(Bullet(*(x+50,y+50)))
+    
+    for bullet in bullets[:]:
+        bullet.update()
+        if not window.get_rect().collidepoint(bullet.pos):
+            bullets.remove(bullet)
+
+    for bullet in bullets:
+        bullet.draw(window)        
     x,y = rotate(x,y)
+
 
 
     pygame.display.flip()
